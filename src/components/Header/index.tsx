f7ce09f28ea8e1b87b18b578913611d8e45a171a
Denis Pimentel Raineri <denis.raineri@nextpage.com.br>
2022-04-27 00:01:11 -0300

    Stripe API, scss componentes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import logo from "../../../public/imagens/images/logo.svg";
+import Image from "next/image";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="imagens/images/logo.svg" alt="" />
+        <Image src={logo} alt="logo" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
